bb622712a1bd6794b4548d39f50c2dd98516d2ac
Matti Aho <ookooko@gmail.com>
2021-03-08 12:41:00 +0200

    cypress

---
+++ cypress/integration/cypress_test.js
@@ -0,0 +1,7 @@
+describe('Puh app', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3001')
+    cy.contains('Phonebook')
+    cy.contains('filter shown with')
+  })
+})
