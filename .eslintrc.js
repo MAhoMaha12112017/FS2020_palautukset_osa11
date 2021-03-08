8a556dc9e4f428fe9aeed2d71803b8140f1859f3
Matti Aho <ookooko@gmail.com>
2021-03-08 12:43:56 +0200

    cypress - eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,14 @@ module.exports = {
         'commonjs': true,
         'es2021': true,
         // 'jest/globals': true,
-        // 'cypress/globals': true
+        'cypress/globals': true
     },
     'extends': [
         'eslint:recommended',
         'plugin:react/recommended'
     ],
     'plugins': [
-        'react'//, 'jest', 'cypress'
+        'react', 'cypress'//, 'jest'
     ],
     'parserOptions': {
         'ecmaFeatures': {
